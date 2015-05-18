604113ff22bf6631e7c3fb26b634df09e621ee7e
Graham Berks <graham@fatlazycat.com>
2015-05-18 22:05:56 +0100

    done comonad

--- src/Course/Comonad.hs
+++ src/Course/Comonad.hs
@@ -1,15 +1,16 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE InstanceSigs        #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE InstanceSigs #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 module Course.Comonad
 (
   Comonad(..)
 ) where
 
-import Course.Core
-import Course.Extend
-import Course.Id
+import           Course.Core
+import           Course.Extend
+import           Course.Id
 
 -- | All instances of the `Comonad` type-class must satisfy two laws. These
 -- laws are not checked by the compiler. These laws are given as:
@@ -19,30 +20,22 @@ import Course.Id
 --
 -- * The law of right identity
 --   `∀f. copure . (f <<=) == f
-class Extend f => Comonad f where
-  copure ::
-    f a
-    -> a
+class Extend f => Comonad f  where
+  copure :: f a -> a
 
 -- | Implement the @Comonad@ instance for @Id@.
 --
 -- >>> copure (Id 7)
 -- 7
 instance Comonad Id where
-  copure ::
-    Id a
-    -> a
-  copure =
-    error "todo"
+  copure :: Id a -> a
+  copure = runId
 
 -- | Witness that all things with (<<=) and copure also have (<$>).
 --
 -- >>> (+10) <$> Id 7
 -- Id 17
-(<$>) ::
-  Comonad f =>
-  (a -> b)
-  -> f a
-  -> f b
-(<$>) =
-  error "todo"
+(<$>) :: Comonad f
+      => (a -> b) -> f a -> f b
+(<$>) fn fa = fun <<= fa
+  where fun = fn . copure
