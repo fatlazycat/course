30f1a7ddf9edf7a0acba00399dd7dda856cb4d61
Graham Berks <graham@fatlazycat.com>
2015-05-20 19:54:09 +0100

    Done compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -1,13 +1,13 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Course.Compose where
 
-import Course.Core
-import Course.Functor
-import Course.Apply
-import Course.Applicative
-import Course.Bind
+import           Course.Applicative
+import           Course.Apply
+import           Course.Bind
+import           Course.Core
+import           Course.Functor
 
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
@@ -15,25 +15,19 @@ newtype Compose f g a =
   Compose (f (g a))
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo"
+-- (a->b) -> Compose f g a -> Compose f g b
+instance (Functor f,Functor g) => Functor (Compose f g) where
+  (<$>) ab (Compose fg) = Compose ((ab <$>) <$> fg)
 
-instance (Apply f, Apply g) =>
-  Apply (Compose f g) where
 -- Implement the (<*>) function for an Apply instance for Compose
-  (<*>) =
-    error "todo"
+-- Compose f g (a->b) -> Compose f g a -> Compose f g b
+instance (Apply f,Apply g) => Apply (Compose f g) where
+  (<*>) (Compose f) (Compose g) = Compose ( (<*>) <$> f <*> g)
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo"
+instance (Applicative f,Applicative g) => Applicative (Compose f g) where
+  pure a = Compose (pure (pure a))
 
-instance (Bind f, Bind g) =>
-  Bind (Compose f g) where
 -- Implement the (=<<) function for a Bind instance for Compose
-  (=<<) =
-    error "todo"
+instance (Bind f,Bind g) => Bind (Compose f g) where
+  (=<<) = error "impossible"
